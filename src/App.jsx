38ec9f9c8aab3cd22e36544310472afbeff4f05a
sunako13 <javlon@example.com>
2025-07-17 14:50:19 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div style={{ 
+      textAlign: 'center', 
+      marginTop: '20%', 
+      fontFamily: 'sans-serif' 
+    }}>
+      <h1>Xush kelibsiz!</h1>
+      <p>Bu mening portfolio saytimgina, hozircha sinov holatida.</p>
+    </div>
+  );
+}
+
+export default App;
